f38ede350d5dcb7bf9763fd278a7efb80ce0ed2c
joyce <joyce.mbt@gmail.com>
2022-10-31 16:24:28 -0300

    criando template da pagina register

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { OptionSelect, Inputs, 
+  FooterAuth} from '../../components'
+import logo from '../../assets/logo.svg';
+import '../login/login.css'
+import '../../style.css'
+import './register.css'
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <section className='div-auth'>
+			<img className='logo' src={logo} alt='logo'></img>
+			<form>
+        <Inputs type='text' placeholder='NOME' class /><br />
+				<Inputs type='email' placeholder='E-MAIL' class /><br />
+				<Inputs type='password' placeholder='SENHA' /><br />
+        <Inputs type='password' placeholder='CONFIRMAR SENHA' /><br />
+        <select className="select-register">
+          <OptionSelect value='Sua função' />
+          <OptionSelect value='Atendente' />
+          <OptionSelect value='Cozinheiro(a)' />
+		    </select> 
+				<Inputs type='submit' value='CADASTRAR' />
+			</form>
+			<FooterAuth text1='Já possui uma conta?' text2='Faça login!' href='/login' />
+		</section> 
   );
 }
