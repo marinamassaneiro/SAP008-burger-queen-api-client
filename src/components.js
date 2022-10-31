fba280579172a01cab8d745b99022d5cc49a1562
joyce <joyce.mbt@gmail.com>
2022-10-31 16:26:23 -0300

    criando um componente para opção do select

--- src/components.js
+++ src/components.js
@@ -9,3 +9,11 @@ export function FooterAuth({ text1, text2, href }) {
 		<p className="footer-auth">{text1} <a href={href}>{text2}</a></p>
 	);
 }
+
+export function OptionSelect({ value }) {
+	return (
+		<option value={value}>{value}</option>
+	)
+}
+
+
